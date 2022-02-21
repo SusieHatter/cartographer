fa46c79d43a66fd2dfab03829af766c82bb0793e
SusieHatter <susiivaldez@gmail.com>
2022-02-21 09:52:45 -0600

    Add reactRouter and fetch map image by id

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,14 @@
-import React, { useState } from "react";
-import { drawLine } from "./draw";
-import useCanvas from "./hooks/useCanvas";
-import useSyncCanvas from "./hooks/useSyncCanvas";
+import { Route, Routes } from "react-router-dom";
+import EditMapPage from "./pages/EditMapPage";
 
-const WIDTH = 3000;
-const HEIGHT = 1500;
-
-function App() {
-  const { ctx, ref } = useCanvas(WIDTH, HEIGHT);
-  useSyncCanvas(ctx, "http://localhost:8090/maps", 5000);
-
-  const [penDown, setPenDown] = useState(false);
-  const [[x, y], setPenPosition] = useState([0, 0]);
-
-  const onMouseDown = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    setPenDown(true);
-    setPenPosition([e.clientX, e.clientY]);
-  };
-
-  const onMouseMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    if (!penDown || !ctx) {
-      return;
-    }
-    const [newX, newY] = [e.clientX, e.clientY];
-    drawLine(ctx, x, y, newX, newY, "black", 1);
-    setPenPosition([newX, newY]);
-  };
-
-  const onMouseUp = () => {
-    setPenDown(false);
-  };
-
+const App = () => {
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
-      <canvas
-        ref={ref}
-        onMouseDown={onMouseDown}
-        onMouseMove={onMouseMove}
-        onMouseUp={onMouseUp}
-      ></canvas>
+    <div>
+      <Routes>
+        <Route path="/maps/:id" element={<EditMapPage />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- server/cmd/cartographer-server/main.go
+++ server/cmd/cartographer-server/main.go
@@ -5,18 +5,35 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/rs/cors"
 	"susie.mx/cartographer/internal/db"
 )
 
+func getId(url string) (int, error) {
+	tokens := strings.Split(url, "/")
+	idStr := tokens[2]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return 0, fmt.Errorf("id is not a string: %s", err)
+	}
+	return id, nil
+}
+
 func mapsHandler(db db.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		log.Println(fmt.Sprintf("%s: %s", req.Method, req.URL))
+		id, err := getId(req.URL.String())
+		if err != nil {
+			log.Println(err)
+			return
+		}
 		switch req.Method {
 		case "GET":
-			mapImage := db.GetMapImage(5)
+			mapImage := db.GetMapImage(id)
 			w.Write(mapImage.Bytes)
 		case "POST":
 			mapBytes, err := ioutil.ReadAll(req.Body)
@@ -24,7 +41,7 @@ func mapsHandler(db db.DB) http.HandlerFunc {
 				log.Println(err)
 				return
 			}
-			db.UpdateMapImage(5, mapBytes)
+			db.UpdateMapImage(id, mapBytes)
 		}
 	}
 }
@@ -32,7 +49,7 @@ func mapsHandler(db db.DB) http.HandlerFunc {
 func main() {
 	db := db.Connect(db.LoadFromEnv())
 	mux := http.NewServeMux()
-	mux.HandleFunc("/maps", mapsHandler(db))
+	mux.HandleFunc("/maps/", mapsHandler(db))
 	handler := cors.Default().Handler(mux)
 	http.ListenAndServe(":8090", handler)
 }
