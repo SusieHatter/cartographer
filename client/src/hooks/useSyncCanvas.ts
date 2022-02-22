49094b6cc9068d4f55abded9980ce5e1c7bb5d3c
SusieHatter <susiivaldez@gmail.com>
2022-02-22 10:58:20 -0600

    Fix race condition in useSyncCanvas

--- client/src/hooks/useSyncCanvas.ts
+++ client/src/hooks/useSyncCanvas.ts
@@ -1,24 +1,30 @@
 import { useEffect } from "react";
 import { getMapImage, updateMapImage } from "../api/mapImages";
+import { MapImage } from "../models";
 
 const loadCanvas = async (
   ctx: CanvasRenderingContext2D,
   mapImageId: number
-) => {
+): Promise<void> => {
   const mapImage = await getMapImage(mapImageId);
+  const dataUrl =
+    mapImage.dataUrl || (await postCanvas(ctx, mapImageId)).dataUrl;
   const img = new Image();
-  img.onload = function () {
-    ctx.drawImage(img, 0, 0);
-  };
-  img.src = mapImage.dataUrl;
+  return new Promise((resolve) => {
+    img.onload = function () {
+      ctx.drawImage(img, 0, 0);
+      resolve();
+    };
+    img.src = dataUrl;
+  });
 };
 
 const postCanvas = async (
   ctx: CanvasRenderingContext2D,
   mapImageId: number
-) => {
+): Promise<MapImage> => {
   const dataUrl = ctx.canvas.toDataURL();
-  await updateMapImage(mapImageId, { dataUrl });
+  return await updateMapImage(mapImageId, { dataUrl });
 };
 
 type Milliseconds = number;
